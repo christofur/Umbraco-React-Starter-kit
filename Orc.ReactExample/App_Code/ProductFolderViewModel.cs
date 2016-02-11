8e883632d1943823a130066b90a5a129d8eedc74
Chris Perks <pop.chris@gmail.com>
2016-02-11 20:54:27 +0000

    Added controllers

---
+++ Orc.ReactExample/App_Code/ProductFolderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Orc.ReactExample.Models
+{
+    public class ProductFolderViewModel
+    {
+        public ProductFolderViewModel()
+        {
+            Products = new List<ProductViewModel>();
+        }
+        public List<ProductViewModel> Products { get; set; }
+    }
+
+  
+}
